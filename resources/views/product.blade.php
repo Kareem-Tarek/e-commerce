9c7b268f3d69765ee338238e7659636658441291
nada5694 <96081397+nada5694@users.noreply.github.com>
2022-07-08 01:53:03 +0300

    ssff

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -621,10 +621,9 @@
     </div>
     <!-- ***** Subscribe Area Ends ***** -->
 
-    <!-- ***** Footer Start ***** -->
-    @include('layouts.footer')
-    <!-- ***** Footer End ***** -->
+
 @endsection
+
 
 @section('scripts')
 
