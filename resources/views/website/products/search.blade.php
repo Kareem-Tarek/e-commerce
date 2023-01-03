fe0034f06d1b5b37eea3a59d5726da3a422d6820
nada5694 <96081397+nada5694@users.noreply.github.com>
2023-01-03 11:40:18 +0300

    Responsive

--- resources/views/website/products/search.blade.php
+++ resources/views/website/products/search.blade.php
@@ -18,14 +18,42 @@
 
 @section('content')
 <style>
-    .session-message{
-        width: 60%;
-        margin-top: 1%;
-        margin-bottom: 3%;
-        margin-left: auto;
-        margin-right: auto;
+    .curriculum-event-thumb a span h3 {
+        font-weight: bolder;
     }
-    /* .table-search-results{margin-left: auto; margin-right: auto;} */
+    .curriculum-event-thumb a img {
+        width: 180px;
+        height: 200px;
+        border: 2px solid black;
+    }
+    .product-results-section {
+        padding: 0% 2%;
+    }
+    .product-results-section .message-result{
+        text-align: center; margin-left: auto; margin-right: auto;  width: 40%;
+    }
+    .product-results-section .message-result span {
+        font-size: 110%; font-weight: bold;
+    }
+    .session-message button {
+        color: rgb(173, 6, 6);
+    }
+    .search-product {
+        display: flex;
+        justify-content: flex-start;
+        text-align: center;
+        flex-wrap: wrap;
+    }
+    .curriculum-event-thumb a span{
+        position: absolute;
+        background: rgba(0, 69, 175, 0.65);
+        width: 180px;
+        height: 35px;
+        font-weight: bold;
+        text-align: center;
+        color: snow; opacity: 0.70;
+    }
+
 </style>
 
 <!-- ***** Search bar Start ***** -->
@@ -33,76 +61,31 @@
 <!-- ***** Search bar End ***** -->
 
 <div id="search-blade" class="search-blade mt-5">
-    <?php /********************************** Start Dashboard Table for Product (for dashboard!) **********************************/ ?>
-    {{-- <table border="1" cellpadding="10" class="table-search-results">
-        <thead>
-            <tr style="padding: 5%;">
-                <th scope = "col">#</th>
-                <th scope = "col">ID</th>
-                <th scope = "col">Title</th>
-                <th scope = "col">Price</th>
-                <th scope = "col">Category</th>
-                @if(auth()->user()->user_type == 'admin')
-                    <th scope = "col">Action</th>
-                @endif
-            </tr>
-        </thead>
-
-        <tbody>
-            @foreach($products_result as $product)
-                <tr>
-                    <th>{{ $loop->iteration}}</th>
-                    <td>{{ $product->id ?? 'Not Found'}}</th>
-                    <td>{{ $product->name ?? 'Not Found' }}</td>
-                    <td>{{ $product->price ?? 'Not Found' }}</td>
-                    <td>{{ $product->product_category ?? '' }}</td>
-                    @if(auth()->user()->user_type == 'admin')
-                        <td style="display: flex;">
-                            <div style="margin-top: 5%;">
-                                <button class="btn btn-success">Edit</button>
-                                <a href="javascript:void(0);" style="background-color: rgb(36, 157, 36); color: snow; padding: 25%; border-radius: 2px;">
-                                    Edit
-                                </a>
-                            </div>
-                            &nbsp;&nbsp;&nbsp;&nbsp;
-                            <button class="btn btn-danger" type="submit">Delete</button>
-                        </td>
-                    @endif
-                </tr>
-            @endforeach
-        </tbody>
-    </table> --}}
-    <?php /********************************** End Dashboard Table for Product (for dashboard!) **********************************/ ?>
-
-    <section class="product-results-section" style="padding: 0% 2%;">
+    <section class="product-results-section">
             @if($search_text_input == "")
-                <div class="alert alert-danger" role="alert" style="text-align: center; margin-left: auto; margin-right: auto;  width: 40%;">
-                    <span style="font-size: 110%; font-weight: bold;">The search box is empty. You didn't enter anything in it!</span>
+                <div class="alert alert-danger message-result" role="alert">
+                    <span>The search box is empty. You didn't enter anything in it!</span>
                 </div>
-            {{-- @elseif($search_text != $products_result)
-                <div class="alert alert-danger" role="alert" style="text-align: center; margin-left: auto; margin-right: auto; margin-bottom: 2%; width: 40%;">
-                    <span style="font-size: 110%; font-weight: bold;">"{{ $search_text }}" Not Found!</span>
-                </div> --}}
             @else <?php //@elseif($search_text == $products_result) ?>
                 @if($products_result_count == 0)
-                    <div class="alert alert-danger" style="text-align: center; margin-left: auto; margin-right: auto; width: 40%;">
+                    <div class="alert alert-danger message-result">
                         "{{ $search_text_input }}" results ({{ $products_result_count }}) - Not found!
                     </div>
                     @auth
                         @if(auth()->user()->user_type == "admin" || auth()->user()->user_type == "moderator" || auth()->user()->user_type == "supplier")
                             <div class="d-flex justify-content-center">
-                                Try to add a new product from&nbsp;<a href="{{route('products.create')}}" class="" style="" type="" title="Add New Product">here</a>.
+                                Try to add a new product from&nbsp;<a href="{{route('products.create')}}" class="" type="" title="Add New Product">here</a>.
                             </div>
                         @endif
-                     @endauth
+                    @endauth
                 @else
-                    <div class="alert alert-success" style="text-align: center; margin-left: auto; margin-right: auto; width: 40%;">
+                    <div class="alert alert-success message-result">
                         "{{ $search_text_input }}" results ({{ $products_result_count }})
                     </div>
                     @auth
                         @if(auth()->user()->user_type == "admin" || auth()->user()->user_type == "moderator" || auth()->user()->user_type == "supplier")
                             <div class="d-flex justify-content-center mb-4">
-                                <a href="{{route('products.create')}}" class="btn btn-dark" style="" type="button" title="Add New Product">Add New Product</a>
+                                <a href="{{route('products.create')}}" class="btn btn-dark" type="button" title="Add New Product">Add New Product</a>
                             </div>
                         @endif
                     @endauth
@@ -110,69 +93,62 @@
 
                 @if(session()->has('addCart_message'))
                     <div class="alert alert-success text-center session-message">
-                        <button type="button" class="close" data-dismiss="alert" style="color: rgb(173, 6, 6)">x</button>
+                        <button type="button" class="close" data-dismiss="alert">x</button>
                         {{ session()->get('addCart_message') }}<a href="{{ route('cart-registered') }}"> Check your cart</a>.
                     </div>
                 @elseif(session()->has('exceeded_available_quantity_message'))
                     <div class="alert alert-danger text-center session-message">
-                        <button type="button" class="close" data-dismiss="alert" style="color: rgb(173, 6, 6)">x</button>
+                        <button type="button" class="close" data-dismiss="alert">x</button>
                         {{ session()->get('exceeded_available_quantity_message') }}
-                    </div> 
+                    </div>
                 @elseif(session()->has('quantity_is_null_message'))
                     <div class="alert alert-danger text-center session-message">
-                        <button type="button" class="close" data-dismiss="alert" style="color: rgb(173, 6, 6)">x</button>
+                        <button type="button" class="close" data-dismiss="alert">x</button>
                         {{ session()->get('quantity_is_null_message') }}
                     </div>
                 @elseif(session()->has('quantity_is_zero_message'))
                     <div class="alert alert-danger text-center session-message">
-                        <button type="button" class="close" data-dismiss="alert" style="color: rgb(173, 6, 6)">x</button>
+                        <button type="button" class="close" data-dismiss="alert">x</button>
                         {{ session()->get('quantity_is_zero_message') }}
                     </div>
                 @elseif(session()->has('quantity_is_negative_message'))
                     <div class="alert alert-danger text-center session-message">
-                        <button type="button" class="close" data-dismiss="alert" style="color: rgb(173, 6, 6)">x</button>
+                        <button type="button" class="close" data-dismiss="alert">x</button>
                         {{ session()->get('quantity_is_negative_message') }}
                     </div>
                 @endif
 
                 @if(session()->has('addRating_message'))
                     <div class="alert alert-success text-center session-message">
-                        <button type="button" class="close" data-dismiss="alert" style="color: rgb(173, 6, 6)">x</button>
+                        <button type="button" class="close" data-dismiss="alert">x</button>
                         {{ session()->get('addRating_message') }}
                     </div>
                 @endif
 
                 @if(session()->has('addFavorite_message'))
                     <div class="alert alert-success text-center session-message">
-                        <button type="button" class="close" data-dismiss="alert" style="color: rgb(173, 6, 6)">x</button>
+                        <button type="button" class="close" data-dismiss="alert">x</button>
                         {{ session()->get('addFavorite_message') }}<a href="{{ route('favorites-registered') }}"> Check your favorites</a>.
                     </div>
                 @elseif(session()->has('addFavorite_already_added_message'))
                     <div class="alert alert-danger text-center session-message">
-                        <button type="button" class="close" data-dismiss="alert" style="color: rgb(173, 6, 6)">x</button>
+                        <button type="button" class="close" data-dismiss="alert">x</button>
                         {{ session()->get('addFavorite_already_added_message') }}
                     </div>
                 @endif
 
-                <div style="display: flex; justify-content: flex-start; text-align: center; flex-wrap: wrap;">
+                <div class="search-product">
                     @forelse($products_result as $product)
                         <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12 mt-3 pt-3 pb-3 bg-light border">
                                 <div class="curriculum-event-thumb">
-                                    {{-- @php $data = Carbon\Carbon::parse($product->created_at)->diffInDays(Carbon\Carbon::now()); @endphp
-                                    @if($data <= 7) <!---------- in days ---------->
-                                        <span style="font-weight: bold; color: rgb(54, 42, 163);">
-                                            <img src="/assets/images/new-icon.png" alt="new_item" style="width: 20%;">
-                                        </span>
-                                    @endif --}}
-
                                     <a href="{{ route('single_product_page' , $product->id) }}">
                                         @php $data = Carbon\Carbon::parse($product->created_at)->diffInDays(Carbon\Carbon::now()); @endphp
                                         @if($data <= 7) <!---------- in days ---------->
-                                            <span class="mt-2" style="position: absolute; background: rgba(0, 69, 175, 0.65); width: 180px; height: 35px; font-weight: bold; text-align: center; color: snow; opacity: 0.70;">
-                                                <h3 style="font-weight: bolder;">NEW</h3>
+                                            <span class="mt-2">
+                                                <h3>NEW</h3>
                                             </span>
                                         @endif
-                                        <img class="mt-2" src="{{$product->image_name}}" alt="{{$product->name}}" style="width: 180px; height: 200px; border: 2px solid black;">
+                                        <img class="mt-2" src="{{$product->image_name}}" alt="{{$product->name}}">
                                     </a>
                                 </div>
                                 <div class="curriculum-event-content d-flex justify-content-center">
@@ -193,7 +169,7 @@
                                                         <span style="color: rgb(59, 188, 59); ">(In-stock)</span>
                                                     @endif
                                                 @endauth
-            
+
                                                 @if(!auth()->user())
                                                     @if($product->available_quantity <= 10 && $product->available_quantity != 0)
                                                         <span style="color: rgb(255, 106, 0);">({{ $product->available_quantity }} only left in-stock)</span>
@@ -221,11 +197,6 @@
                                                     @else
                                                         Sports Wear
                                                     @endif
-
-                                                    {{-- {{ $product->clothing_type }} --}}
-
-                                                    {{-- @inject('categories_clothing_type','App\Models\Category')
-                                                    {!! Form::select('name',$categories_clothing_type->pluck('name','id')) !!} --}}
                                                 </div>
                                             @else <!-- elseif($product->is_accessory == 'yes') -->
                                                 <!-- empty -->
@@ -234,17 +205,12 @@
                                     </div>
                                 </div>
                                 <div class="mt-2 mb-2" style="color:rgb(72, 125, 171);">
-                                    (Total Ratings: {{ \App\Models\Rating::where('product_id', $product->id)->count() }}) 
+                                    (Total Ratings: {{ \App\Models\Rating::where('product_id', $product->id)->count() }})
                                 </div>
                                 @auth
                                     @if(auth()->user()->user_type == 'admin')
                                         @include('layouts.website.admin-product-control-website')
                                     @elseif(auth()->user()->user_type == 'customer')
-                                        {{-- <div style="margin-top: 4%; margin-bottom: 5%;">
-                                            <a href="{{ route('cart-registered') }}" style="background-color: rgb(48, 116, 217); color: snow; padding: 1.5% 3%; border-radius: 4px;">
-                                                Add Cart
-                                            </a>
-                                        </div> --}}
                                         <div style="width: 70%; margin-left: auto; margin-right: auto;">
                                             @include('layouts.website.addCart-form')
                                             @include('layouts.website.addRating-form')
@@ -261,9 +227,7 @@
                                 @endif
                         </div>
                         @empty
-                        {{-- <div class="alert alert-danger" role="alert" style="text-align: center; margin-left: auto; margin-right: auto; margin-top: 2%; width: 40%">
-                            <span>{{ $search_text }} not found!</span>
-                        </div>
+
                         <?php
                             /*
                             Note:
@@ -275,7 +239,7 @@
                             @empty => acts like else from the if condition and for showing the other choice wich will be the error
                             (or the undefined data from the DB) if the data wasn't found in the code in "forelse" loop.
                             */
-                        ?> --}}
+                        ?>
                     @endforelse
                 </div>
             @endif
