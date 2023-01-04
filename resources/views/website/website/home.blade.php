f19f2f8b814e3eeb54f4fd89d95b589614ed0269
nada5694 <96081397+nada5694@users.noreply.github.com>
2023-01-04 09:04:44 +0300

    Responsive

--- resources/views/website/website/home.blade.php
+++ resources/views/website/website/home.blade.php
@@ -297,7 +297,7 @@
 
                                         @if(Auth::guest())
                                             <div class="guest-operations">
-                                                <a class="add-to-cart-btn guest-cart-btn" href="{{ route('cart-unregistered') }}">Add To Cart</a>
+                                                <a class="add-to-cart-btn" href="{{ route('cart-unregistered') }}">Add To Cart</a>
                                                 <a class="add-to-favorites-btn" href="{{ route('favorites-unregistered') }}">Add To Favorites</a>
                                             </div>
                                         @endif
